5e332a76eb9e2aa4d28eb28f746bd4fde098abf6
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-06-06 08:36:11 +0000

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    n = len(my_list)
+    n = len(my_list)-1
     if idx <= 0 or idx > n:
         return my_list
     else:
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    i = len(my_list)
+    i = len(my_list)-1
     if idx <= 0 or idx > i:
         return my_list
     else:
