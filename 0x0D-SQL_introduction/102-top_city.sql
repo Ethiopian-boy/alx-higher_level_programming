ca434fa60cb6f59b8b4a023e568c4efb99af2f6e
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-08-09 19:41:38 +0000

    SQL - Introduction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,7 +2,7 @@
 -- ordered by temperature (descending)
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE month = 6 AND month = 9
+WHERE month > 6 AND month > 9
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
