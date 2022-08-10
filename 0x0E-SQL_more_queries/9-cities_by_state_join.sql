4182a68592ad94d9c4bc42b01ca9376cf880e35f
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-08-10 18:55:59 +0000

    SQL - More queries

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- script that lists all cities contained in the database hbtn_0d_usa
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+SELECT cities.id, cities.name, state.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = state.id
+ORDER BY cities.id ASC;
