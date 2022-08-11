222d3bc1b56ba7e91370f2166403ae23c0399377
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-08-11 20:25:55 +0000

    fix my code'

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,16 +1,12 @@
 -- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 SELECT DISTINCT tvs.title
 FROM tv_shows tvs
-INNER JOIN tv_show_genres it
-ON tvs.id = it.genre_id
-INNER JOIN tv_genres tvg
-ON tvg.id = it.show_id
 WHERE tvs.title NOT IN (
 	SELECT tvs.title
 	FROM tv_shows tvs
 	INNER JOIN tv_show_genres it
-	ON tvs.id = it.genre_id
+	ON tvs.id = it.show_id
 	INNER JOIN tv_genres tvg
-	ON tvg.id = it.show_id
+	ON tvg.id = it.genre_id
 	WHERE name = 'Comedy')
 ORDER BY tvs.title ASC;
