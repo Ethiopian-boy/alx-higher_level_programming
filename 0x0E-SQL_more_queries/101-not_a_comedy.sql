b7ba8cc662d47cad21b3d4aeaca5bc181161c749
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-08-11 20:06:26 +0000

    SQL - More queries

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,16 @@
+-- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+SELECT DISTINCT tvs.title
+FROM tv_shows tvs
+INNER JOIN tv_show_genres it
+ON tvs.id = it.genre_id
+INNER JOIN tv_genres tvg
+ON tvg.id = it.show_id
+WHERE tvs.title NOT IN (
+	SELECT tvs.title
+	FROM tv_shows tvs
+	INNER JOIN tv_show_genres it
+	ON tvs.id = it.genre_id
+	INNER JOIN tv_genres tvg
+	ON tvg.id = it.show_id
+	WHERE name = 'Comedy')
+ORDER BY tvs.title ASC;
