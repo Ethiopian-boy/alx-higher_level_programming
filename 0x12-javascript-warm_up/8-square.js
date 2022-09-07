3112ea1ccb1cc0ed2ae1367aaba4a30fa14971e1
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-09-07 04:06:13 +0000

    a script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,6 +5,6 @@ if (isNaN(argInt)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < argInt; i++) {
-    console.log('x'.repeat(argInt));
+    console.log('X'.repeat(argInt));
   }
 }
