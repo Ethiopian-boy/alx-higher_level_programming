ed233c48604236a39b52eee63bfc164bb8465c70
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-09-07 03:56:07 +0000

    Inheritance

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -7,7 +7,8 @@
 const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
-    supeiisize, size);
+    super(size, size);
   }
 }
+
 module.exports = Square;
