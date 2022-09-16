cc791234895403bc13afa847e9e5994f1d5a266b
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-09-16 13:36:07 +0000

    City relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -22,7 +22,10 @@ if __name__ == "__main__":
 
     new_state = State(name='California')
     new_city = City(name='San Francisco', state=new_state)
-    session.add(new_city)
-    session.commit()
+    new_state.cities.append(new_city)
 
+    session.add(new_state)
+    session.add(new_city)
+
+    session.commit()
     session.close()
