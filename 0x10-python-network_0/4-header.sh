2a64036f1be26f03278fb403a506f0b1c917d9a2
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-09-22 17:45:12 +0000

    cURL

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes URL, add header variable, displays "Hello School!"; Usage: ./4-header.sh 0.0.0.0:5000/route_5 ; echo ""
-curl -s -H "X-School-User-Id: 98" $1
+curl -sX GET -H "X-School-User-Id: 98" "$1"
