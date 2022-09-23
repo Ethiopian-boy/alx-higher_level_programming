78e3e3148086b740a6a3697b4b5785db999d2893
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-09-23 17:32:45 +0000

    fix some errors'

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     url = argv[1]
     req = Request(url)
     try:
-        with urlopen(req) as responsse:
+        with urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except HTTPError as e:
         print("Error code: {}".format(e.code))
